a0227a1d275ed2fe4011da6e2e3bcfa1e4ba35ac
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-10-25 14:59:47 -0300

    tipos de Workers: ever, once, interval e debound

--- lib/workers/workers_controller.dart
+++ lib/workers/workers_controller.dart
@@ -11,16 +11,55 @@ class WorkersController extends GetxController {
 //!bom para filtro de lista
   @override
   void onInit() {
-    final everWork = ever(
+    //*Primeiro Worker
+    //!ele peca TUDOOO a cada segundo
+    // final everWork = ever(
+    //   _nome,
+    //   (nome) {
+    //     print('Executando o worker Ever');
+    //     print(nome);
+    //   },
+    //   cancelOnError: true,
+    //   condition: () => _nome.value == "dario",
+    // );
+    // _workers.add(everWork);
+    // super.onInit();
+
+    //*Segundo Worker
+    //* Faz apenas 1x na primeira letra
+    // final onceWorker = once<String>(_nome, (nome) {
+    //   print('Executando o worker Once');
+    //   print(nome);
+    // });
+    // _workers.add(onceWorker);
+    // super.onInit();
+
+    //*Terceiro Worker
+    //*ignora todas as requisiçoes pelo TEMPO que foi determinado pelo usuario
+    // final workerInterval = interval(
+    //   _nome,
+    //   (nome) {
+    //     print(
+    //         '########################### Worker Interval #######################');
+    //     print('###########################$nome #######################');
+    //   }, time: const Duration(seconds: 2) //time: 2.seconds
+    // );
+    // //para excluir sempre que sai da tela
+    // _workers.add(workerInterval);
+    // /super.onInit();
+
+//*Quarto Worker DEBOUNCER
+////!O melhor de todos!!!
+//todo Ele espera alguns segundos eu terminar de digitar e então ele faz a busca!!!!!!!
+//*(800milisegundos), possivel mudar com timer
+    final workDebounce = debounce<String>(
       _nome,
       (nome) {
-        print('Executando o worker Ever');
-        print(nome);
+        print('################## Worker Debounce ####################');
+        print('################## $nome ####################');
       },
-      cancelOnError: true,
-      condition: () => _nome.value == "dario",
     );
-    _workers.add(everWork);
+    _workers.add(workDebounce);
     super.onInit();
   }
 
